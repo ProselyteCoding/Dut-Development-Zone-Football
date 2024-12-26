0283f87b3902c0c41853f05643933be0668299ce
ProselyteCoding <3197908785@qq.com>
2024-12-26 10:49:45 +0800

    modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/index.jsx 	modified:   client/src/pages/Home/Feature/Feature.css 	modified:   client/src/pages/Home/Feature/Feature.jsx

--- client/src/pages/Home/Feature/Feature.jsx
+++ client/src/pages/Home/Feature/Feature.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 import './Feature.css'
 import feature from '../../../assets/images/feature.jpg'
 import texture from '../../../assets/images/texture.png'
+import {init} from 'ityped'
+import { useEffect, useRef } from 'react'
 
 const Feature = () => {
+  const textRef = useRef(null);
+  useEffect(() => {
+    if (textRef.current) {
+      init(textRef.current, {
+        strings: ['享受足球', '挥洒汗水', '收获友谊', '赢得荣誉'],
+        typeSpeed: 150,
+        backSpeed: 150,
+        backDelay: 1700,
+        loop: true,
+        showCursor: false,
+      });
+    }
+  }, []);
+
   return (
     <div className='feature-container'>
       <div className='feature-image-container'>
@@ -21,7 +37,7 @@ const Feature = () => {
       </div>
       <div className='typewriter-container'>
         <span className='typewriter-fixed'>在这里</span>
-        <span className='typewriter-text'></span>
+        <span className='typewriter-text' ref={textRef}></span>
       </div>
     </div>
   )
